c8a69bb8584cdfd2937b7eeb4f4de12844f0700e
Saina Firooz <saina.firooz@gmail.com>
2024-02-20 17:23:07 +0100

    Create order for products

--- main.js
+++ main.js
@@ -10,9 +10,6 @@ import { sampleOffers } from "./sampleData.js";
 
 let supplierModel_collection = SupplierModel.collection;
 
-// await connect("mongodb://127.0.0.1:27017/mms_assignment_2");
-
-
 const connectToDB = async () => {
   try {
     await connect("mongodb://127.0.0.1:27017/mms_assignment_2");
@@ -152,9 +149,7 @@ async function addNewProduct() {
 async function productsByCategory() {}
 
 // menu option 4
-async function productsBySupplier() {
-}
-
+async function productsBySupplier() {}
 
 // menu option 5
 async function viewAllOffers() {
@@ -164,46 +159,160 @@ async function viewAllOffers() {
         type: "input",
         name: "minPrice",
         message: "Enter minimum price",
-        validate: value => !isNaN(value) ? true : 'Please enter a number',
+        validate: (value) => (!isNaN(value) ? true : "Please enter a number"),
+        default: 0,
       },
       {
         type: "input",
         name: "maxPrice",
         message: "Enter maximum price",
-        validate: value => !isNaN(value) ? true : 'Please enter a number',
+        validate: (value) => (!isNaN(value) ? true : "Please enter a number"),
+        default: 2000,
       },
     ]);
 
     const filteredOffers = await OfferModel.aggregate([
       {
         $match: {
-          price: { $gte: Number(minPrice), $lte: Number(maxPrice) }
-        }
-      }
+          price: { $gte: Number(minPrice), $lte: Number(maxPrice) },
+        },
+      },
     ]);
 
     filteredOffers.forEach((offer, index) => {
-      console.log(`Offer ${index + 1}:`);
-      console.log(`Products: ${offer.products.join(', ')}`);
-      console.log(`Price: $${offer.price}`);
-      console.log(`Active: ${offer.active ? 'Yes' : 'No'}`);
-      console.log('------------------------');
+      console.log(
+        `All offers within a price range:\nOffer ${
+          index + 1
+        }:\nProducts: ${offer.products.join(", ")}\nPrice: $${
+          offer.price
+        }\nActive: ${offer.active ? "Yes" : "No"}\n------------------------`
+      );
     });
   } catch (error) {
     console.log(error);
   }
 }
 
-
-
 // menu option 6
-async function specificCategory() {}
+
+async function specificCategory() {
+  try {
+    const allCategories = await ProductModel.aggregate([
+      { $group: { _id: "$category" } },
+    ]);
+
+    const { category } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "category",
+        message: "Choose a category",
+        choices: allCategories.map((category) => category._id).concat("Exit"),
+      },
+    ]);
+
+    if (category === "Exit") {
+      return;
+    }
+
+    const offersContainingCategory = await OfferModel.aggregate([
+      {
+        $match: {
+          $or: [{ category: category }, { category: { $in: [category] } }],
+        },
+      },
+      {
+        $unwind: "$products",
+      },
+      {
+        $group: {
+          _id: "$_id",
+          price: { $first: "$price" },
+          active: { $first: "$active" },
+          products: { $push: "$products" },
+        },
+      },
+    ]);
+
+    if (offersContainingCategory.length === 0) {
+      console.log(`No offers found for category: ${category}`);
+      return;
+    }
+
+    offersContainingCategory.forEach((offer, index) => {
+      console.log(
+        `Offer ${index + 1}:\nPrice: $${offer.price} \nActive: ${
+          offer.active ? "Yes" : "No"
+        }\nIncluded Products: ${offer.products.join(
+          ", "
+        )}\n------------------------`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // menu option 7
 async function productsInStock() {}
 
 // menu option 8
-async function orderForProducts() {}
+async function orderForProducts() {
+  try {
+    const allProducts = await ProductModel.aggregate([
+      {
+        $group: { _id: "$name" },
+      },
+    ]);
+    const { product } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "product",
+        message: "Choose a product",
+        choices: allProducts.map((product) => product._id).concat("Exit"),
+      },
+    ]);
+
+    if (product === "Exit") {
+      return;
+    } else {
+      const { quantity, status, additionalDetails } = await inquirer.prompt([
+        {
+          type: "input",
+          name: "quantity",
+          message: "Enter quantity: ",
+          validate: (value) => (!isNaN(value) ? true : "Please enter a number"),
+          default: 1,
+        },
+        {
+          type: "input",
+          name: "status",
+          message: "Enter status: ",
+          default: "pending",
+        },
+        {
+          type: "input",
+          name: "additionalDetails",
+          message: "Enter additional details: ",
+          default: "N/A",
+        },
+      ]);
+
+      const newOrder = new SalesOrderModel({
+        order: product,
+        quantity: quantity,
+        status: status,
+        orderadditional_detail: additionalDetails,
+      });
+      
+
+      await newOrder.save();
+      console.log(`Order for ${product} has been created\n ${newOrder}`);
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // menu option 9
 async function orderForOffers() {}
@@ -212,10 +321,7 @@ async function orderForOffers() {}
 async function shipOrders() {}
 
 // menu option 11
-async function addNewSupplier() {   
-      
-}
-
+async function addNewSupplier() {}
 
 // menu option 12
 async function viewSuppliers() {}
@@ -236,8 +342,6 @@ async function closeDBconnection() {
 }
 
 (async () => {
-    await connectToDB();
-    await menu();
-  })();
-  
-
+  await connectToDB();
+  await menu();
+})();
