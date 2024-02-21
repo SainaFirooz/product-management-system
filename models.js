3ef72c4708f6af47bf03c62ea1c17e23df45057b
Saina Firooz <saina.firooz@gmail.com>
2024-02-21 17:06:52 +0100

    string array removed

--- models.js
+++ models.js
@@ -26,8 +26,7 @@ const ProductSchema = mongoose.Schema({
 
 const SalesOrdersSchema = mongoose.Schema({
   order: {
-    type: [String],
-    required: true,
+    type: String,
   },
   offer: {
     type: String,
