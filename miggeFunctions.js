8dadb71de7188a153610e642ff1052571be17090
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2024-02-21 17:35:44 +0100

    started fucntions migge

--- miggeFunctions.js
+++ miggeFunctions.js
@@ -1,1 +1,29 @@
-console.log('Sup');
+export const addNewCategory = async ()  => {
+    try{
+        const { category_choice } = await inquirer.prompt([
+          {
+            type: 'input',
+            name: 'category_choice',
+            message: 'Enter new category',
+          },
+          {
+            type: 'list',
+            message: [CategoryModel]
+          }
+        ]);
+        console.log('You´ve entered', category_choice );
+    
+      }catch(error){
+        console.log(error);
+      }
+}
+
+
+// menu option 3
+async function productsByCategory() {}
+
+// menu option 4
+async function productsBySupplier() {}
+
+// menu option 12
+async function viewSuppliers() {}
