3fed02fd639d145ad6c58f71ab9f3a40d0ac50e0
Saina Firooz <saina.firooz@gmail.com>
2024-02-25 13:08:02 +0100

    console log / last changes

--- miggeFunctions.js
+++ miggeFunctions.js
@@ -36,7 +36,13 @@ export const addNewCategory = async () => {
         newCategory.name = category_name;
         newCategory.description = category_description;
         category_collection.insertOne(newCategory);
-        console.log("You´ve inserted a new category", newCategory);
+        console.log(`\nNew Category Details:\n
+        Name: ${newCategory.name}
+        Description: ${newCategory.description}
+        \n---------------------------------`);
+        console.log(
+          "You've inserted a new category successfully!\n---------------------------------"
+        );
         break;
       } else {
         console.log(
@@ -56,34 +62,41 @@ export async function productsByCategory() {
 
   const categoryChoices = allCategories.map((category) => category.name);
 
-  console.log(categoryChoices);
   const categoryAnswer = await inquirer.prompt([
     {
       type: "list",
       name: "category",
       message: "Select a category",
-      choices: categoryChoices,
+      choices:  [...categoryChoices, "Exit"]
     },
+
   ]);
+
+  if (categoryAnswer.category === "Exit") {
+    return;
+  } 
 
   const products = await ProductModel.find({
     "category.name": categoryAnswer.category,
+    
   });
 
   if (!products.length) {
     console.log("No products found for this category");
     return;
   }
-
+  console.log(`\n Here are the products for the category: ${categoryAnswer.category} \n`);
+  
   products.forEach((product) => {
-    console.log(`Name: ${product.name}`);
-    console.log(`Price: ${product.price}`);
-    console.log(`Cost: ${product.cost}`);
-    console.log(`Supplier: ${product.supplier.name}`);
-    console.log(`Category: ${product.category.name}`);
-    console.log(`Stock Quantity: ${product.stock}`);
-    console.log("---------------------------------");
-  });
+    console.log(`\n${product.name}\n
+        Price: $${product.price}
+        Cost: $${product.cost}
+        Supplier: ${product.supplier.name}
+        Category: ${product.category.name}
+        Stock Quantity: ${product.stock}
+        \n---------------------------------`);
+}
+  );
 }
 
 // menu option 4
@@ -97,51 +110,51 @@ export async function productsBySupplier() {
       type: "list",
       name: "supplier",
       message: "Select a supplier",
-      choices: supplierChoices,
+      choices: [...supplierChoices, "Exit"]
     },
   ]);
 
+  if (supplierAnswer.supplier === "Exit") {
+    return;
+  }
+
   const products = await ProductModel.find({
     "supplier.name": supplierAnswer.supplier,
-  });
+  }).populate('supplier').populate('category');
 
-  if (!products.lenght) {
+  if (!products.length) {
     console.log("No products found for this supplier");
   }
 
+  console.log(`\n Here are the products for the supplier: ${supplierAnswer.supplier} \n`);
+
   products.forEach((product) => {
-    console.log(`Name: ${product.name}`);
-    console.log(`Price: ${product.price}`);
-    console.log(`Cost: ${product.cost}`);
-    console.log(`Supplier: ${product.supplier.name}`);
-    console.log(`Category: ${product.category.name}`);
-    console.log(`Stock Quantity: ${product.stock}`);
-    console.log("---------------------------------");
+    console.log(`\n${product.name}\n
+        Price: $${product.price}
+        Cost: $${product.cost}
+        Supplier: ${product.supplier.name}
+        Category: ${product.category.name}
+        Stock Quantity: ${product.stock}
+        \n---------------------------------`);
   });
+  console.log(
+    "Product details have been displayed successfully!\n---------------------------------"
+  );
 }
 
 // menu option 12
 export async function viewSuppliers() {
-  // const allSuppliers = await SupplierModel.aggregate([
-  //     {
-  //         $project: {_id: 0, name: 1, contact: 1}
-  //     }
-  // ]);
-
-  // allSuppliers.forEach(supplier => {
-  //     console.log(`Supplier: ${supplier.name} \nContact info: ${supplier.contact.name} - ${supplier.contact.email}`);
-  // })
-
-  // const allSuppliers = await SupplierModel.find({}, 'name')
-  // const supplierListName = allSuppliers.map(supplier => {
-  //     console.log(supplier.name);
-  // })
   const suppliers = await SupplierModel.find({});
 
   suppliers.forEach((supplier) => {
-    console.log(`Name: ${supplier.name}`);
-    console.log(`Contact Person: ${supplier.contact.name}`);
-    console.log(`Email: ${supplier.contact.email}`);
-    console.log("---------------------------------");
+    console.log(`\nSupplier Details:\n
+        Name: ${supplier.name}
+        Contact Person: ${supplier.contact.name}
+        Email: ${supplier.contact.email}
+        \n---------------------------------`);
   });
+
+  console.log(
+    "Supplier details have been displayed successfully!\n---------------------------------"
+  );
 }
